12d247e611efde7f8f59164f620b2d85ba84d2f3
Vicky-tbpc <vickyhuang@tbpchc.com>
2026-03-18 14:52:37 +0800

    Update gemini02.js

--- api/gemini02.js
+++ api/gemini02.js
@@ -1,4 +1,4 @@
-// api/gemini.js 13
+// api/gemini.js 14
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
